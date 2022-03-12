5643c686920e5163b3d183b5060772373d70734a
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-03-12 13:32:04 +0530

    home page design complete

---
+++ src/components/Home.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ReactAudioPlayer from 'react-audio-player';
+
+
+export const Home = () => {
+
+    const [songs , setSongs] = useState([]);
+    const [alone , setAlone] = useState('');
+    const [auto , setAuto] = useState(false);
+
+    const getSongs = () => {
+
+        axios.get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/studiod9c0baf.json')
+           .then((data) => {
+           
+            console.log(data.data);
+            setSongs(data.data);
+            setAlone(data.data[0]);
+    
+           })
+           .catch((err) => console.log(err.message))  
+   }
+
+   const handleClick = (e) => {
+       setAlone(e);
+       setAuto(true);
+       console.log('yes');
+   }
+
+useEffect(()=> {
+ getSongs();
+} , []);
+
+
+if(songs.length === 0) {
+    return (
+        <div>loading....</div>
+    )
+}
+
+    return(
+
+     
+ 
+        <div id='box'>
+
+        <div id='songs'>
+
+          {songs.map((e,i) => (
+  
+           <div id='songsbox' key={i} onClick={() => handleClick(e)}>
+
+             <img src={e.cover_image} alt=''/> 
+
+           <div id='color-c'>
+           <strong><p>{e.song}</p></strong>
+           <p>{e.artists} </p>
+          </div>
+
+        
+            {/* <ReactAudioPlayer
+             src={e.url}
+             controls
+             id='play'
+             /> */}
+
+           </div>
+
+          ))}
+
+        </div>
+
+
+<div id='side'>
+
+<img src={alone.cover_image} alt=''/> 
+
+<div>
+<p>{alone.artists} </p>
+ <p>{alone.song}</p>
+
+ <ReactAudioPlayer
+   src={alone.url}
+   controls
+   autoPlay={auto}
+   id='play'
+   />
+
+</div>
+     
+</div>
+
+</div>
+
+        
+       
+    )
+}
